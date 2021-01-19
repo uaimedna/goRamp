2dfe023c1a7cf41c8c82282d8bddd63e43cf6c62
justas <uaimedna@gmail.com>
2021-01-19 22:53:25 +0200

    migrations

---
+++ database/migrations/2021_01_19_202820_create_company_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->softDeletes(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
